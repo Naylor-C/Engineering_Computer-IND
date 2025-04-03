5053015348d7488aa40d1088eb788a3b8b0cae6e
Jhonatan <95220392+Naylor-C@users.noreply.github.com>
2025-04-03 02:38:19 +0000

    x

--- main/src/main.cc
+++ main/src/main.cc
@@ -9,16 +9,25 @@ protected:
   // Signal handlers:
   void on_button_clicked();
 
+  Gtk::Box VBox;
+
+  Gtk::Entry LText;
+  LPass;
+
   Gtk::Button LB;
 };
 
-MyWindow::MyWindow()
+MyWindow::MyWindow() : VBox(Gtk::Orientation::VERTICAL)
 {
   set_title("Basic application");
   set_default_size(800, 600);
 }
 
-//LB = Button;
+MyWindow::MyWindow() : LText("Username || Email");
+{
+}
+
+// LB = Button;
 MyWindow::MyWindow()
     : LB("Login");
 {
@@ -28,21 +37,18 @@ MyWindow::MyWindow()
   // When the button receives the "clicked" signal, it will call the
   // on_button_clicked() method defined below.
   LB.signal_clicked().connect(sigc::
-  mem_fun(*this,
-          &MyWindow::on_button_clicked));
+                                  mem_fun(*this,
+                                          &MyWindow::on_button_clicked));
 
   // This packs the button into the Window (a container).
   set_child(LB);
 }
 
-
-//Evento de Click Do button
+// Evento de Click Do button
 void MyWindow::on_button_clicked()
 {
-  
 }
-//end event
-
+// end event
 
 int main(int argc, char *argv[])
 {
